9de19cf99ed82beaa07a8a057c9be4eef0c998f7
Alexander Tselkovnikov <leks511@bk.ru>
2022-11-26 22:29:28 +0300

    #Module3-task1 Added routes and private route

---
+++ project/src/pages/page-404/page-404.tsx
@@ -0,0 +1,11 @@
+function Page404(): JSX.Element {
+  return (
+    <main className="page__main">
+      <div className="container">
+        404
+      </div>
+    </main>
+  );
+}
+
+export default Page404;
