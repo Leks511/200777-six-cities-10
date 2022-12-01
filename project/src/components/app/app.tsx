44cdf8d568486ff080fd87b34eee7e8787d4bda0
Alexander Tselkovnikov <leks511@bk.ru>
2022-12-01 20:45:21 +0300

    #module3-task1 Fixed JSX in pages components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,7 +4,7 @@ import Favorites from '../../pages/favorites/favorites';
 import Main from '../../pages/main/main';
 import Page404 from '../../pages/page-404/page-404';
 import Login from '../../pages/login/login';
-import PlaceCard from '../place-card/place-card';
+import Room from '../../pages/room/room';
 import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
@@ -15,18 +15,28 @@ function App({offersCount}: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Root} element={<Main offersCount={offersCount} />} />
+        <Route
+          path={AppRoute.Root}
+          element={
+            <Main offersCount={offersCount} />
+          }
+        />
         <Route path={AppRoute.Login} element={<Login />} />
         <Route
           path={AppRoute.Favorites}
           element={
-            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
               <Favorites />
             </PrivateRoute>
           }
         />
         <Route path={AppRoute.Offer}>
-          <Route path=':id' element={<PlaceCard />}/>
+          <Route
+            path=':id'
+            element={
+              <Room />
+            }
+          />
         </Route>
         <Route path={AppRoute.NotFoundScreen} element={<Page404 />}/>
       </Routes>
--- project/src/pages/page-404/page-404.tsx
+++ project/src/pages/page-404/page-404.tsx
@@ -1,10 +1,23 @@
 function Page404(): JSX.Element {
   return (
-    <main className="page__main">
-      <div className="container">
-        404
-      </div>
-    </main>
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="page__main">
+        <div className="page__login-container container">
+          404 Page not found
+        </div>
+      </main>
+    </div>
   );
 }
 
