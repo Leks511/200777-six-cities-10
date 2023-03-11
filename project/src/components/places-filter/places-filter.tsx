9b29371dab811aa96a523ba03fb0c7ca07041a90
Leks <leks511@bk.ru>
2023-03-11 12:28:35 +0300

    Added filter component for offers

--- project/src/components/places-filter/places-filter.tsx
+++ project/src/components/places-filter/places-filter.tsx
@@ -1,19 +1,58 @@
-const PlacesFilter = (): JSX.Element => (
-  <form className="places__sorting" action="#" method="get">
-    <span className="places__sorting-caption">Sort by</span>
-    <span className="places__sorting-type" tabIndex={0}>
-              Popular
-      <svg className="places__sorting-arrow" width="7" height="4">
-        <use xlinkHref="#icon-arrow-select"></use>
-      </svg>
-    </span>
-    <ul className="places__options places__options--custom places__options--opened">
-      <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-      <li className="places__option" tabIndex={0}>Price: low to high</li>
-      <li className="places__option" tabIndex={0}>Price: high to low</li>
-      <li className="places__option" tabIndex={0}>Top rated first</li>
-    </ul>
-  </form>
-);
+import { useState } from 'react';
+import cn from 'classnames';
+
+const filters = [
+  'Popular',
+  'Price: low to high',
+  'Price: high to low',
+  'Top rated first'
+];
+
+const PlacesFilter = (): JSX.Element => {
+  const [isOpenedFilter, setIsOpenedFilter] = useState<boolean>(false);
+  const [activeFilter, setActiveFilter] = useState<string>(filters[0]);
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+      <button
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setIsOpenedFilter(!isOpenedFilter)}
+      >
+        {activeFilter}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </button>
+      <ul className={cn(
+        'places__options',
+        'places__options--custom',
+        {'places__options--opened': isOpenedFilter}
+      )}
+      >
+        {
+          filters.map((variant) => (
+            <li
+              key={variant}
+              tabIndex={0}
+              className={cn(
+                'places__option',
+                {'places__option--active': activeFilter === variant}
+              )}
+              onClick={() => {
+                setActiveFilter(variant);
+                setIsOpenedFilter(false);
+              }}
+            >
+              {variant}
+            </li>
+          )
+          )
+        }
+      </ul>
+    </form>
+  );
+};
 
 export default PlacesFilter;
